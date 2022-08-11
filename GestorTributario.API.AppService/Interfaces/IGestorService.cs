b0b387f75443cd608ef9d12e6451fee1106c4ecf
Juliano Braz <jbraz@bionexo.com>
2022-08-10 23:41:15 -0300

    fix(gestorTributario): return type

--- GestorTributario.API.AppService/Interfaces/IGestorService.cs
+++ GestorTributario.API.AppService/Interfaces/IGestorService.cs
@@ -1,9 +1,10 @@
+using System.Threading.Tasks;
 using GestorTributario.API.AppService.Models.Parameters;
 
 namespace GestorTributario.API.AppService.Interfaces
 {
     public interface IGestorService<T>
     {
-        T ConsultarTributacaoProduto(ConsultaParametros parametros);
+       Task<T> ConsultarTributacaoProduto(ConsultaParametros parametros);
     }
 }
