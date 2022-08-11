ca4469a7db6326f6ce843473c8df0d98388eaf8a
Juliano Braz <jbraz@bionexo.com>
2022-08-10 23:20:55 -0300

    fix: add required to consulta parametro

--- GestorTributario.API.AppService/Models/Parameters/ConsultaParametros.cs
+++ GestorTributario.API.AppService/Models/Parameters/ConsultaParametros.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GestorTributario.API.AppService.Models.Parameters
 {
     public class ConsultaParametros
     {
+        [Required]
         public int CodigoProduto { get; set; }
     }
 }
